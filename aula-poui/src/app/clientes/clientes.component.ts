5b2458a6e777d1bd3f3e78f8ae05cf1d8599a4d2
ricardocostaadvtec <ricardocosta.advtec@gmail.com>
2021-01-22 19:50:59 -0300

    tabela de clientes

--- aula-poui/src/app/clientes/clientes.component.ts
+++ aula-poui/src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PoPageAction,PoBreadcrumbModule  ,PoTableColumn, PoBreadcrumb, PoTableAction } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-clientes',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
+  actions: Array<PoPageAction> = [
+    {
+      label:'Incluir',
+      url:'home/clientes/new',
+    },
+  ];
+  breadcrumb: PoBreadcrumb ={
+    items: [{label:'Home', link:'/home'}, {label:'Cliente'}],
+    };
+    items:any;
+
+
+  colunas: Array<PoTableColumn>=[
+    {property: 'id', label: 'ID', type:'string'},
+    {property: 'nome', label: 'Nome', type:'string'},
+    {property: 'endereco', label: 'Endereco', type:'string'},
+    {property: 'dataNascimento', label: 'Data Nascimento', type:'date'},
+  ];
+
+  tableActions: Array<PoTableAction> = [
+    { action: this.visualizar.bind(this), label:'Visualizar' },
+    { action: this.editar.bind(this), label:'Editar' },
+  ];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  visualizar(){
+
   }
+
+  editar(){
+
+  }
+
+
 
 }
